45f9fc40de51b7a260b5008bb008d0197c6a0031
Sotiris Maipas <sotiris.maipas@gmail.com>
2020-02-15 23:39:42 +0200

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer v-model="drawer" temporary app>
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in menuItems"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer app>
+      <span>&copy; {{ new Date().getFullYear() }} Toulis Apartments</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      menuItems: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Contact',
+          to: '/contact',
+        },
+      ],
+      title: 'Vuetify.js',
+    };
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout column>
+    <v-flex xs12 sm8 md6>
+      homepage
+      <v-btn color="primary">test</v-btn>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+/**
+ * Home page.
+ */
+export default {
+  name: 'Index',
+};
+</script>
